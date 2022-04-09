9427f8e39d041db4b1eb6dcb7817a747d3e45b5c
KeuryLendof <lendofkeury@gmail.com>
2022-04-09 18:11:59 -0400

    funcionalidad del inventario

--- app/app.js
+++ app/app.js
@@ -4,17 +4,41 @@ let aumentarCafeBtn = document.getElementById("aumentarCafe"),
 
     aumentarAzucarBtn = document.getElementById("aumentarAzucar"),
     disminuirAzucarBtn = document.getElementById("disminuirAzucar"),
-    azucarBtn = document.getElementById("cantidadAzucar");
+    azucarBtn = document.getElementById("cantidadAzucar"),
+
+    aumentarVasoPBtn = document.getElementById("aumentarVasoP"),
+    disminuirVasoPBtn = document.getElementById("disminuirVasoP"),
+    vasoPBtn = document.getElementById("cantidadVasoP"),
+
+    aumentarVasoMBtn = document.getElementById("aumentarVasoM"),
+    disminuirVasoMBtn = document.getElementById("disminuirVasoM"),
+    vasoMBtn = document.getElementById("cantidadVasoM"),
+
+    aumentarVasoGBtn = document.getElementById("aumentarVasoG"),
+    disminuirVasoGBtn = document.getElementById("disminuirVasoG"),
+    vasoGBtn = document.getElementById("cantidadVasoG"),
+
+    dialog = document.getElementById("dialog"),
+    cerrar = document.getElementById("cerrar"),
+    show = document.getElementById("abrirLogin");
 
 
 
 let cafe = localStorage.getItem("cafe");
 let azucar = localStorage.getItem("azucar");
+let vasoPequeno = localStorage.getItem("vasoPequeno");
+let vasoMediano = localStorage.getItem("vasoMediano");
+let vasoGrande = localStorage.getItem("vasoGrande");
 
 function cargar(){
-    //localStorage.setItem("azucar", 5);
+    // localStorage.setItem("vasoPequeno", 10);
+    // localStorage.setItem("vasoMediano", 10);
+    // localStorage.setItem("vasoGrande", 10);
     document.getElementById('cantidadCafe').innerHTML = cafe;
     document.getElementById('cantidadAzucar').innerHTML = azucar;
+    document.getElementById('cantidadVasoP').innerHTML = vasoPequeno;
+    document.getElementById('cantidadVasoM').innerHTML = vasoMediano;
+    document.getElementById('cantidadVasoG').innerHTML = vasoGrande;
 }
 
 aumentarCafeBtn.addEventListener("click",()=>{
@@ -40,6 +64,45 @@ disminuirAzucarBtn.addEventListener("click",()=>{
     azucarBtn.innerHTML = azucar
     localStorage.setItem("azucar", azucar)
 })
+
+aumentarVasoPBtn.addEventListener("click",()=>{
+    vasoPequeno += 1
+    vasoPBtn.innerHTML = vasoPequeno
+    localStorage.setItem("vasoPequeno", vasoPequeno)
+})
+
+disminuirVasoPBtn.addEventListener("click",()=>{
+    vasoPequeno -= 1
+    vasoPBtn.innerHTML = vasoPequeno
+    localStorage.setItem("vasoPequeno", vasoPequeno)
+})
+
+aumentarVasoMBtn.addEventListener("click",()=>{
+    vasoMediano += 1
+    vasoMBtn.innerHTML =  vasoMediano
+    localStorage.setItem("vasoMediano",  vasoMediano)
+})
+
+disminuirVasoMBtn.addEventListener("click",()=>{
+    vasoMediano -= 1
+    vasoMBtn.innerHTML =  vasoMediano
+    localStorage.setItem("vasoMediano",  vasoMediano)
+})
+
+aumentarVasoGBtn.addEventListener("click",()=>{
+    vasoGrande += 1
+    vasoGBtn.innerHTML =  vasoGrande
+    localStorage.setItem("vasoGrande",  vasoGrande)
+})
+
+disminuirVasoGBtn.addEventListener("click",()=>{
+    vasoGrande -= 1
+    vasoGBtn.innerHTML =  vasoGrande
+    localStorage.setItem("vasoGrande",  vasoGrande)
+})
+
+show.addEventListener('click', () => dialog.showModal())
+cerrar.addEventListener('click', () => dialog.close())
 
 
 // localStorage.setItem("titulo", "Curso de Angular avanzado - Víctor Robles");
