d10c6259d720d591392bac1ad164993c728b0e9a
KeuryLendof <lendofkeury@gmail.com>
2022-04-09 16:04:31 -0400

    almacenamiento local

--- app/app.js
+++ app/app.js
@@ -1,15 +1,49 @@
-let incrementarBtn = document.getElementById("aumentar"),
-    disminuirBtn = document.getElementById("disminuir"),
-    counterBtn = document.getElementById("counter");
+let aumentarCafeBtn = document.getElementById("aumentarCafe"),
+    disminuirCafeBtn = document.getElementById("disminuirCafe"),
+    cafeBtn = document.getElementById("cantidadCafe"),
 
-let contador = 0
+    aumentarAzucarBtn = document.getElementById("aumentarAzucar"),
+    disminuirAzucarBtn = document.getElementById("disminuirAzucar"),
+    azucarBtn = document.getElementById("cantidadAzucar");
 
-incrementarBtn.addEventListener("click",()=>{
-    contador += 1
-    counterBtn.innerHTML = contador
+
+
+let cafe = localStorage.getItem("cafe");
+let azucar = localStorage.getItem("azucar");
+
+function cargar(){
+    //localStorage.setItem("azucar", 5);
+    document.getElementById('cantidadCafe').innerHTML = cafe;
+    document.getElementById('cantidadAzucar').innerHTML = azucar;
+}
+
+aumentarCafeBtn.addEventListener("click",()=>{
+    cafe += 1
+    cafeBtn.innerHTML = cafe
+    localStorage.setItem("cafe", cafe)
 })
 
-disminuirBtn.addEventListener("click",()=>{
-    contador -= 1
-    counterBtn.innerHTML = contador
+disminuirCafeBtn.addEventListener("click",()=>{
+    cafe -= 1
+    cafeBtn.innerHTML = cafe
+    localStorage.setItem("cafe", cafe)
 })
+
+aumentarAzucarBtn.addEventListener("click",()=>{
+    azucar += 1
+    azucarBtn.innerHTML = azucar
+    localStorage.setItem("azucar", azucar)
+})
+
+disminuirAzucarBtn.addEventListener("click",()=>{
+    azucar -= 1
+    azucarBtn.innerHTML = azucar
+    localStorage.setItem("azucar", azucar)
+})
+
+
+// localStorage.setItem("titulo", "Curso de Angular avanzado - Víctor Robles");
+
+// localStorage.getItem("titulo");
+
+// localStorage.removeItem("titulo");
